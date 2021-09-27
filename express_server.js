153636cfecaadfd0d4f08ebc731b5dcc9493f7d7
Joseph Shin <seoung.shin@ontariotechu.net>
2021-09-27 17:14:31 -0400

    added body parser and URLS_new ejs

--- express_server.js
+++ express_server.js
@@ -8,9 +8,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
